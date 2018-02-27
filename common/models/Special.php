494883917f200d8b69162ccfa2a46f928b5c3298
lsp12588 <lsp12588@126.com>
2018-02-27 11:18:31 +0800

    special

--- common/models/Special.php
+++ common/models/Special.php
@@ -5,7 +5,7 @@ namespace common\models;
 use Yii;
 
 /**
- * This is the model class for table "zq_special".
+ * This is the model class for table "zq_special".专业
  *
  * @property int $id
  * @property string $title 标题
