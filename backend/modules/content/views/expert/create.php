9d2093c3f285daa00b2b2f540b4fe78482573a1a
597050481@qq.com <597050481@qq.com>
2018-02-27 09:33:23 +0800

    expert admin

--- backend/modules/content/views/expert/create.php
+++ backend/modules/content/views/expert/create.php
@@ -6,13 +6,11 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\Expert */
 
-$this->title = 'Create Expert';
-$this->params['breadcrumbs'][] = ['label' => 'Experts', 'url' => ['index']];
+$this->title = '新增专家';
+$this->params['breadcrumbs'][] = ['label' => '专家管理', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="expert-create">
-
-    <h1><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [
         'model' => $model,
