cc5b861a2a99fff5bfc0f98000aeac0091c43497
liyy-k <1441511838@qq.com>
2018-03-16 15:15:38 +0800

    frontend update

--- common/config/params.php
+++ common/config/params.php
@@ -3,4 +3,15 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
+    
+    'sms_config'=>[
+        'soap_server_url'=>'http://sms.glodon.com/SMSService.asmx?wsdl',//服务地址
+        'namespace'=>'http://www.glodon.com/',//命名空间
+        'soap_action'=>'http://www.glodon.com/SendSms',//SOAPAction
+        'soap_defencoding'=>'UTF-8',//编码
+        'wsdl'=>'wsdl',//wsdl
+        'username'=>'zfxc',//账户名
+        'passwoed'=>'zfxc20130807',//密码
+        'soap_request_action'=>'SendSms',//接口方法
+    ],
 ];
