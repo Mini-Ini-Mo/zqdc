5c5b6c3ac0d8c2a1595bd3bc466e2a2691b6f76a
liyy-k <1441511838@qq.com>
2018-03-27 15:19:20 +0800

    update

--- backend/modules/content/controllers/ActLessonController.php
+++ backend/modules/content/controllers/ActLessonController.php
@@ -9,7 +9,7 @@ use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 use yii\web\Response;
-use common\models\Actless;
+use common\models\ActLess;
 use yii\base\BaseObject;
 
 /**
