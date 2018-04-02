c8617fe278190ccfe31c67c10916f3fe9cac4fc4
manbanpai <761846158@qq.com>
2018-04-02 15:58:54 +0800

    un

--- common/widgets/yii2_wechat/src/UnifiedOrder.php
+++ common/widgets/yii2_wechat/src/UnifiedOrder.php
@@ -29,7 +29,8 @@ class UnifiedOrder extends Base
 
     //构建json参数
     /*
-     * "appId":"wx2421b1c4370ec43b",//公众号名称，由商户传入"timeStamp":"1395712654",//时间戳，自1970年以来的秒数
+     * "appId":"wx2421b1c4370ec43b",//公众号名称，由商户传入
+     * "timeStamp":"1395712654",//时间戳，自1970年以来的秒数
      * "nonceStr":"e61463f8efa94090b1f366cccfbbb444", //随机串
      * "package":"prepay_id=u802345jgfjsdfgsdg888",
      * "signType":"MD5",//微信签名方式：
