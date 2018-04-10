baa9d9456edd46f1c73e27a1ee3a0ab0042282cf
liyy-k <1441511838@qq.com>
2018-04-10 13:42:51 +0800

    update

--- frontend/views/layouts/base.php
+++ frontend/views/layouts/base.php
@@ -28,52 +28,7 @@ AppAsset::register($this);
 <?php $this->beginBody() ?>
 
 <div class="wrap">
-    <?php
-    \yii\bootstrap\NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => '#',
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
 
-    $menuItems = [
-        /* ['label' => '首页', 'url' => Yii::$app->homeUrl], */
-        [
-            'label' => '中清商学',
-            'items' => [
-                ['label' => '中清筑道', 'url' => ['/lessons/index']],
-                ['label' => '中清博纳', 'url' => ['/lessons/offline']],
-                ['label' => '中清游学', 'url' => ['/study-abroad/index']],
-                ['label' => '中清论坛', 'url' => ['/forum/index']],
-    
-            ],
-        ],
-        //['label' => '专题', 'url' => ['/special/index']],
-        [
-            'label' => '中清智库',
-            'items' => [
-                //['label' => '智库案例', 'url' => ['/special/index']],
-                ['label' => '智库创新营', 'url' => ['/special/index']],
-    
-            ],
-        ],
-        ['label' => '新领袖', 'url' => ['/expert/index']],
-    ];
-    
-    if (!Yii::$app->user->isGuest) {
-        array_unshift($menuItems,['label' => '个人中心', 'url' => ['/member/index']]);
-    }else{
-        array_push($menuItems,['label' => '注册', 'url' => ['/site/signup']],['label' => '登录', 'url' => ['/site/login']]);
-    }
-    
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => $menuItems,
-    ]);
-    
-    \yii\bootstrap\NavBar::end();
-    ?>
     
     <div class="container">
         <?= Breadcrumbs::widget([
