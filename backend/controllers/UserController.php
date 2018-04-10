c9ba41907fc06401fd1997f88abf8bd5655057a6
liyy-k <1441511838@qq.com>
2018-04-10 10:47:35 +0800

    update

--- backend/controllers/UserController.php
+++ backend/controllers/UserController.php
@@ -75,9 +75,10 @@ class UserController extends Controller
         $model = new SignupForm();
         if ($model->load(Yii::$app->request->post())) {
             if ($user = $model->signup()) {
-                if (Yii::$app->getUser()->login($user)) {
-                    return $this->goHome();
-                }
+                /* if (Yii::$app->getUser()->login($user)) {
+                    
+                } */
+                return $this->goHome();
             }
         }
         
